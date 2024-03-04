fd42f25e94ccf525f100f3361933e7d678d19f90
Shadman Abedin <sabedin@apple.com>
2024-03-04 13:04:29 -0800

    completions - starting to add some snippets in

--- lua/completions.lua
+++ lua/completions.lua
@@ -1,15 +1,19 @@
+require("luasnip.session.snippet_collection").clear_snippets "all"
+require("luasnip.session.snippet_collection").clear_snippets "rust"
+require("luasnip.session.snippet_collection").clear_snippets "typescriptreact"
+
 local lspkind = require("lspkind")
 lspkind.init()
 
 local cmp = require("cmp")
 cmp.setup({
 	mapping = {
-		["<C-k>"] = cmp.mapping.select_next_item(),
-		["<C-j>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-e>"] = cmp.mapping.close(),
-		["<C-y>"] = cmp.mapping.confirm({
+		["<C-h>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Insert,
 			select = true,
 		}),
@@ -20,7 +24,7 @@ cmp.setup({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "path" },
-		{ name = "buffer",  keyword_length = 5 },
+		{ name = "buffer", keyword_length = 5 },
 	},
 	snippet = {
 		expand = function(args)
@@ -46,9 +50,9 @@ cmp.setup({
 	},
 })
 
-local luasnip = require("luasnip")
+local ls = require("luasnip")
 local types = require("luasnip.util.types")
-luasnip.config.setup({
+ls.config.setup({
 	history = true,
 	updateevents = "TextChanged,TextChangedI",
 	ext_opts = {
@@ -58,5 +62,64 @@ luasnip.config.setup({
 			},
 		},
 	},
+})
+
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	end
+end)
+
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+	end
+end, { silent = true })
+
+vim.keymap.set("i", "<c-l>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end)
+
+vim.keymap.set("n", "<leader>so", "<cmd>source ~/.config/nvim/lua/completions.lua<CR>")
+
+local s, i, t, c, f = ls.s, ls.insert_node, ls.text_node, ls.choice_node, ls.function_node
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+local l = require("luasnip.extras").lambda
+
+ls.add_snippets("lua", {
+	s("req", fmt("local {} = require('{}')", { i(1, "default"), rep(1) })),
+})
+
+ls.add_snippets("rust", {
+	s(
+		"modtest",
+		fmt(
+			[[
+	 #[cfg(test)]
+	 mod test {{
+	 {}
+
+		{}
+	 }}
+	]],
+			{
+				c(1, { t("	use super::*"), t("") }),
+				i(0),
+			}
+		)
+	),
+})
+
+ls.add_snippets("typescriptreact", {
+	s(
+		"us",
+		fmt(
+			[[const [{}, set{setter}] = useState({});]],
+			{ i(1), i(2), setter = l(l._1:sub(1, 1):upper() .. l._1:sub(2, -1), 1) }
+		)
+	),
 })
 
