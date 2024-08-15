7faceede85ade997358bf64b3eebe9041faa2476
Shadman Abedin <sabedin@apple.com>
2024-08-15 12:03:43 -0700

    adding templ lsp support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup({
-				ensure_installed = { "black", "stylua", "eslint_d", "prettier" },
+				ensure_installed = { "black", "stylua", "eslint_d", "prettier", "templ" },
 			})
 		end,
 	},
@@ -11,7 +11,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "pyright", "tailwindcss", "gopls" },
+				ensure_installed = { "lua_ls", "tsserver", "pyright", "tailwindcss", "gopls", "rust_analyzer", "templ" },
 			})
 		end,
 	},
@@ -24,6 +24,10 @@ return {
 			lspconfig.pyright.setup({})
 			lspconfig.tailwindcss.setup({})
 			lspconfig.gopls.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.templ.setup({})
+
+			vim.filetype.add({ extension = { templ = "templ" } })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
